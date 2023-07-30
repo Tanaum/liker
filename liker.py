8fd5c55b6610a998490a3a8f23d978f3ca230844
kate-ig <135850652+kate-ig@users.noreply.github.com>
2023-07-30 21:32:03 +0500

    Update liker.py

--- liker.py
+++ liker.py
@@ -2,12 +2,15 @@ import pyautogui,time
 
 time.sleep(10)
 
+#to find out the co-ordinates
 #print(pyautogui.position())
 
 n=0
 
 while n < 5:
+    #it clicks at a certain place
     pyautogui.click(x=739, y=564)
+    #presses right to go to the next post
     pyautogui.press('right') 
     n+=1   
     time.sleep(3)
